9c1314db15f1c92e12fd6831571ef8f7feecf1e0
Kunal Sarkar <kunal@example.com>
2026-06-07 23:23:08 +0530

    Use stdout logging for Vercel

--- src/logger/__init__.py
+++ src/logger/__init__.py
@@ -1,29 +1,8 @@
 import logging
-import os
-from pathlib import Path
-
-from src.constant.training_pipeline import (
-    ARTIFACT_DIR,
-    LOG_DIR,
-    LOG_FILE,
-    PIPELINE_NAME,
-)
-
-BASE_DIR = Path(__file__).resolve().parents[2]
-
-logs_path = os.path.join(
-    str(BASE_DIR),
-    PIPELINE_NAME,
-    ARTIFACT_DIR,
-    LOG_DIR
-)
-
-os.makedirs(logs_path, exist_ok=True)
-
-LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+import sys
 
 logging.basicConfig(
-    filename=LOG_FILE_PATH,
+    stream=sys.stdout,
     format="[ %(asctime)s ] %(name)s - %(levelname)s - %(message)s",
-    level=logging.DEBUG,
+    level=logging.INFO,
 )
